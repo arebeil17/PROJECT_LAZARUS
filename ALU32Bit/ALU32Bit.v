953349af94247f850a8c51aa2fe8bd8878466c76
stealth07 <andres07@email.arizona.edu>
2016-10-07 19:01:08 -0700

    ALU32bit UPDATE 2: PHASE 1 COMPLETE
    The ALU has all phase 1 requirements implemented. Just have to test it
    with the controller hooked up.

--- ALU32Bit/ALU32Bit.v
+++ ALU32Bit/ALU32Bit.v
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-
 ////////////////////////////////////////////////////////////////////////////////
 // Computer Architecture
 // 
@@ -21,6 +20,7 @@
 // corresponding result of the operation based on the 32-Bit inputs, 'A', and 
 // 'B'. The 'Zero' flag is high when 'ALUResult' is '0'. The 'ALUControl' signal 
 // should determine the function of the ALU based on the table below:-
+// ALL PHASE 1 OPERATIONS ARE LISTED BELOW
 // Op   	| 'ALUControl' value
 // ==========================
 // ADD  	| 00000
@@ -39,42 +39,174 @@
 // MOVN 	| 01101
 // MOVZ 	| 01110
 // ROTRV	| 01111
-// ROTR 	| 10000  *SAME AS SRL
-// SRA  	| 10001
-// SRAV 	| 10010
-// SLTU 	| 10011
-// MUL  	| 10100
-// MADD 	| 10101
-// MSUB 	| 10110
-// SEH_SEB  | 10111
+// SRA  	| 10000
+// SRAV 	| 10001
+// SLTU 	| 10010
+// MUL  	| 10011
+// MADD 	| 10100
+// MSUB 	| 10101
+// SEH_SEB  | 10110
 // 
 // NOTE:-
 // SLT (i.e., set on less than): ALUResult is '32'h000000001' if A < B.
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
-module ALU32Bit(ALUControl, A, B, ALUResult, Zero);
+module ALU32Bit(ALUControl, A, B, SRL_Select, ALUResult, Zero);
 
 	input [4:0] ALUControl; // control bits for ALU operation
 	input [31:0] A, B;	    // inputs
-
+	input SRL_Select;       //21bit from instruction used for selecting ROTR or SRL
+    
+    reg [31:0] Hi = 0, Lo = 0;
+      
 	output reg [31:0] ALUResult;	// answer
 	output Zero;	    // Zero=1 if ALUResult == 0
-
-    always @(A, B, ALUControl) begin
-        if(ALUControl == 4'b0010)           //ADD
-            ALUResult =  A + B;
-        else if(ALUControl == 4'b0110)      //SUB
-            ALUResult =  A - B;
-        else if(ALUControl == 4'b0000)      //AND
-            ALUResult =  A & B;
-        else if(ALUControl == 4'b0001)      //OR
-            ALUResult =  A | B;
-        else if(ALUControl == 4'b0111)      //SLT
-            ALUResult =  (A < B) ? (1):(0);
+    
+    localparam [4:0] ADD  =  'b00000,
+                     ADDU  = 'b00001,
+                     SUB   = 'b00010,
+                     MULT  = 'b00011,
+                     MULTU = 'b00100,
+                     AND   = 'b00101,
+                     OR    = 'b00110,
+                     NOR   = 'b00111,
+                     XOR   = 'b01000,
+                     SLL   = 'b01001,
+                     SRL   = 'b01010,
+                     SLLV  = 'b01011,
+                     SLT   = 'b01100,
+                     MOVN  = 'b01101,
+                     MOVZ  = 'b01110,
+                     ROTRV = 'b01111,
+                     SRA   = 'b10000,
+                     SRAV  = 'b10001,
+                     SLTU  = 'b10010,
+                     MUL   = 'b10011,
+                     MADD  = 'b10100,
+                     MSUB  = 'b10101,
+                     SEH_SEB = 'b10110;
+    
+    reg [4:0] Operation;
+    reg [31:0] temp_1 = 0, temp_2 = 0;
+    reg [63:0] temp64 = 0;
+    
+    always @(A, B, ALUControl, Operation) begin
+        case(Operation)
+            ADD: begin
+                ALUResult <= $signed(A) + $signed(B);
+            end
+            ADDU: begin
+                ALUResult <= A + B;
+            end
+            SUB: begin
+                ALUResult <= $signed(A) - $signed(B);
+            end
+            MULT: begin
+                temp64 = $signed(A) * $signed(B);
+                Hi <= temp64[63:32];
+                Lo <= temp64[31:0];
+                ALUResult <= 0; //No ALU Result defaults to zero;
+            end
+            MULTU: begin
+                temp64 = A * B;
+                Hi <= temp64[63:32];
+                Lo <= temp64[31:0];
+                ALUResult <= 0; //No ALU Result defaults to zero;
+            end
+            AND: begin
+                ALUResult <= A & B;
+            end
+            OR: begin
+                ALUResult <= A | B;
+            end
+            NOR: begin
+                ALUResult <= ~(A | B);
+            end
+            XOR: begin
+                ALUResult <= A ^ B;
+            end
+            SLL: begin
+                ALUResult <= A << B;
+            end
+            SRL: begin
+                if(!SRL_Select) begin
+                    ALUResult <= A >> B;
+                end
+                else begin
+                    temp_1 = A >> B;
+                    if(B > 0) begin
+                        temp_2 = A << (32 - B);
+                        temp_1 = temp_1 | temp_2;
+                    end
+                    ALUResult <= temp_1;
+                end
+            end
+            SLLV: begin
+                ALUResult <= A << B;
+            end
+            SLT: begin
+                assign ALUResult = ($signed(A) < $signed(B)) ? (1):(0); 
+            end
+            MOVN: begin
+                if(B != 0) begin
+                    ALUResult <= A;
+                end
+            end
+            MOVZ: begin
+                if(B == 0)
+                    ALUResult <= A;
+            end
+            ROTRV: begin
+                temp_1 = A >> B;
+                if(B > 0) begin
+                    temp_2 = A << (32 - B);
+                    temp_1 = temp_1 | temp_2;
+                end
+                ALUResult <= temp_1;
+            end
+            SRA: begin //Shift right arithmetic
+                assign ALUResult = $signed(A) >>> B;
+            end
+            SRAV: begin
+                assign ALUResult = $signed(A) >>> B;
+            end
+            SLTU: begin
+                assign ALUResult = (A < B) ? (1):(0);
+            end
+            MUL: begin
+                ALUResult <= ($signed(A) * $signed(B));
+            end
+            MADD: begin
+                temp64 = $signed(A) * $signed(B);
+                Hi <= temp64[63:32] + Hi;
+                Lo <= temp64[31:0] + Lo;
+                ALUResult <= 0; //No ALU Result defaults to zero;
+            end
+            MSUB: begin
+                temp64 = $signed(A) * $signed(B);
+                Hi <= temp64[63:32] - Hi;
+                Lo <= temp64[31:0] - Lo;
+                ALUResult <= 0; //No ALU Result defaults to zero;
+            end
+            SEH_SEB: begin
+                if(B[15] == 1)     //First check half word sign extend required
+                    assign ALUResult = (B | 'hffff0000);
+                else if(B[7] == 1) //Else check if byte sign extend required
+                    assign ALUResult = (B | 'hffffff00);
+                else               //Else no sign extend required
+                    ALUResult <= B;
+            end
+            default:
+                ALUResult <= 0;
+        endcase
     end
     
     assign Zero = (ALUResult == 0) ? (1):(0);
+    
+    always @(ALUControl, A, B, SRL_Select)begin
+        Operation <= ALUControl;
+    end
 
 endmodule
 
--- ALU32Bit/ALU32Bit_tb.v
+++ ALU32Bit/ALU32Bit_tb.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 
 ////////////////////////////////////////////////////////////////////////////////
-// Computer Architecture
+// ECE369 - Computer Architecture
 // 
 // Module - ALU32Bit_tb.v
 // Description - Test the 'ALU32Bit.v' module.
@@ -11,6 +11,7 @@ module ALU32Bit_tb();
 
 	reg [4:0] ALUControl;   // control bits for ALU operation
 	reg [31:0] A, B;	        // inputs
+	reg SRL_Select;
 
 	wire [31:0] ALUResult;	// answer
 	wire Zero;	        // Zero=1 if ALUResult == 0
@@ -18,33 +19,181 @@ module ALU32Bit_tb();
     ALU32Bit u0(
         .ALUControl(ALUControl), 
         .A(A), 
-        .B(B), 
+        .B(B),
+        .SRL_Select(SRL_Select), 
         .ALUResult(ALUResult), 
         .Zero(Zero)
     );
+    
+    localparam [4:0] ADD  =  'b00000,
+                     ADDU  = 'b00001,
+                     SUB   = 'b00010,
+                     MULT  = 'b00011,
+                     MULTU = 'b00100,
+                     AND   = 'b00101,
+                     OR    = 'b00110,
+                     NOR   = 'b00111,
+                     XOR   = 'b01000,
+                     SLL   = 'b01001,
+                     SRL   = 'b01010,
+                     SLLV  = 'b01011,
+                     SLT   = 'b01100,
+                     MOVN  = 'b01101,
+                     MOVZ  = 'b01110,
+                     ROTRV = 'b01111,
+                     SRA   = 'b10000,
+                     SRAV  = 'b10001,
+                     SLTU  = 'b10010,
+                     MUL   = 'b10011,
+                     MADD  = 'b10100,
+                     MSUB  = 'b10101,
+                     SEH_SEB = 'b10110;
 
 	initial begin
-	   ALUControl <= 4'b0010;// ADD  | 0010
-	   A <= 0; B <= 0; #50;
-	   A <= 2; B <= 2; #50;
-	   A <= 15; B <= 0; #50;
-	   A <= 15; B <= 10; #50;
-       ALUControl <= 4'b0110;// SUB  | 0110
-       A <= 0; B <= 0; #50;
-       A <= 2; B <= 2; #50;
-       A <= 15; B <= 5; #50;
-       A <= 5; B <= 15; #50;
-       ALUControl <= 4'b0000;// AND  | 0000
-       A <= 0; B <= 0; #50;
-       A <= 2; B <= 2; #50;
-       A <= 15; B <= 0; #50;
-       ALUControl <= 4'b0001;// OR   | 0001
-       A <= 0; B <= 0; #50;
-       A <= 15; B <= 0; #50;
-       ALUControl <= 4'b0111;// SLT  | 0111
-       A <= 0; B <= 0; #50;
-       A <= 15; B <= 0; #50;
-       A <= 0; B <= 15; #50;
+	   //Test ADD
+	       ALUControl <= ADD; A <= 0; B <= 0; SRL_Select <= 0;
+	   #50                    A <= 1; B <= 1; SRL_Select <= 0;
+	   #50                    A <= 2; B <= -2; SRL_Select <= 0;
+	   #50                    A <= 10; B <= -15; SRL_Select <= 0;
+	   #50                    A <= 64; B <= 64; SRL_Select <= 0;
+	   //Test ADDU
+       #50 ALUControl <= ADDU; A <= 0; B <= 0; SRL_Select <= 0;
+       #50                     A <= 1; B <= 1; SRL_Select <= 0;
+       #50                     A <= 2; B <= -2; SRL_Select <= 0;
+       #50                     A <= 10; B <= 'hfffffff0; SRL_Select <= 0;
+       #50                     A <= 64; B <= 64; SRL_Select <= 0;
+	   //Test SUB
+	   #50 ALUControl <= SUB; A <= 0; B <= 4; SRL_Select <= 0;
+       #50                    A <= 1; B <= 1; SRL_Select <= 0;
+       #50                    A <= 2; B <= -2; SRL_Select <= 0;
+       #50                    A <= 10; B <= -15; SRL_Select <= 0;
+       #50                    A <= 10; B <= 11; SRL_Select <= 0;
+       //Test MULT
+       #50 ALUControl <= MULT; A <= 0; B <= 4; SRL_Select <= 0;
+       #50                     A <= 1; B <= 1; SRL_Select <= 0;
+       #50                     A <= 2; B <= -2; SRL_Select <= 0;
+       #50                     A <= 10; B <= -15; SRL_Select <= 0;
+       #50                     A <= 10; B <= 11; SRL_Select <= 0;
+       //Test MULTU
+       #50 ALUControl <= MULTU; A <= 0; B <= 4; SRL_Select <= 0;
+       #50                      A <= 1; B <= 1; SRL_Select <= 0;
+       #50                      A <= 2; B <= -2; SRL_Select <= 0;
+       #50                      A <= 10; B <= -15; SRL_Select <= 0;
+       #50                      A <= 10; B <= 11; SRL_Select <= 0;
+       //Test AND
+       #50 ALUControl <= AND; A <= 0; B <= 0; SRL_Select <= 0;
+       #50                    A <= 1; B <= 1; SRL_Select <= 0;
+       #50                    A <= 0; B <= 6; SRL_Select <= 0;
+       #50                    A <= 14; B <= 6; SRL_Select <= 0;
+       #50                    A <= 2021; B <= 1; SRL_Select <= 0;
+       //Test OR
+       #50 ALUControl <= OR; A <= 0; B <= 0; SRL_Select <= 0;
+       #50                    A <= 1; B <= 0; SRL_Select <= 0;
+       #50                    A <= 4; B <= 4; SRL_Select <= 0;
+       #50                    A <= -4; B <= -4; SRL_Select <= 0;
+       #50                    A <= 2021; B <= 0; SRL_Select <= 0;
+       //Test NOR
+       #50 ALUControl <= NOR; A <= 0; B <= 0; SRL_Select <= 0;
+       #50                    A <= 1; B <= 0; SRL_Select <= 0;
+       #50                    A <= 4; B <= 4; SRL_Select <= 0;
+       #50                    A <= 'hfffffff0; B <= 0; SRL_Select <= 0;
+       #50                    A <= 2021; B <= 0; SRL_Select <= 0;
+       //Test XOR
+       #50 ALUControl <= XOR; A <= 0; B <= 0; SRL_Select <= 0;
+       #50                    A <= 1; B <= 0; SRL_Select <= 0;
+       #50                    A <= 0; B <= 4; SRL_Select <= 0;
+       #50                    A <= 2; B <= 4; SRL_Select <= 0;
+       #50                    A <= 2; B <= 2; SRL_Select <= 0;
+       //Test SLL
+       #50 ALUControl <= SLL; A <= 4; B <= 1; SRL_Select <= 0;
+       #50                    A <= 4; B <= 2; SRL_Select <= 0;
+       #50                    A <= 4; B <= 3; SRL_Select <= 0;
+       #50                    A <= 8; B <= 31; SRL_Select <= 0;
+       #50                    A <= 1024; B <= 1; SRL_Select <= 0;
+       //Test SLLV
+       #50 ALUControl <= SLLV; A <= 4; B <= 1; SRL_Select <= 0;
+       #50                     A <= 4; B <= 2; SRL_Select <= 0;
+       #50                     A <= 4; B <= 3; SRL_Select <= 0;
+       #50                     A <= 8; B <= 31; SRL_Select <= 0;
+       #50                     A <= 1024; B <= 1; SRL_Select <= 0;
+       //Test SRL
+       #50 ALUControl <= SRL; A <= 128; B <= 1; SRL_Select <= 0;
+       #50                    A <= 128; B <= 2; SRL_Select <= 0;
+       #50                    A <= 128; B <= 3; SRL_Select <= 0;
+       #50                    A <= 256; B <= 1; SRL_Select <= 0;
+       #50                    A <= 256; B <= 10; SRL_Select <= 0;
+       //Test ROTR
+       #50 ALUControl <= SRL; A <= 1; B <= 1; SRL_Select <= 1;
+       #50                    A <= 15; B <= 4; SRL_Select <= 1;
+       #50                    A <= 1; B <= 32; SRL_Select <= 1;
+       #50                    A <= 4; B <= 2; SRL_Select <= 1;
+       #50                    A <= 16; B <= 5; SRL_Select <= 1;
+       //Test SLT
+       #50 ALUControl <= SLT;   A <= 1; B <= 1; SRL_Select <= 0;
+       #50                      A <= -1; B <= 1; SRL_Select <= 0;
+       #50                      A <= 64; B <= 4; SRL_Select <= 0;
+       #50                      A <= -64; B <= 2; SRL_Select <= 0;
+       #50                      A <= 0; B <= -4; SRL_Select <= 0;
+       //Test MOVN
+       #50 ALUControl <= MOVN; A <= 1; B <= 0; SRL_Select <= 0;
+       #50                     A <= 15; B <= 0; SRL_Select <= 0;
+       #50                     A <= 1; B <= 1; SRL_Select <= 0;
+       #50                     A <= 4; B <= 1; SRL_Select <= 0;
+       #50                     A <= -16; B <= 1; SRL_Select <= 0;
+       //Test MOVZ
+       #50 ALUControl <= MOVZ; A <= 1; B <= 0; SRL_Select <= 0;
+       #50                     A <= 15; B <= 0; SRL_Select <= 0;
+       #50                     A <= 1; B <= 1; SRL_Select <= 0;
+       #50                     A <= 4; B <= 1; SRL_Select <= 0;
+       #50                     A <= -16; B <= 1; SRL_Select <= 0;
+       //Test ROTRV
+       #50 ALUControl <= ROTRV; A <= 1; B <= 1; SRL_Select <= 0;
+       #50                      A <= 15; B <= 4; SRL_Select <= 0;
+       #50                      A <= 1; B <= 32; SRL_Select <= 0;
+       #50                      A <= 4; B <= 2; SRL_Select <= 0;
+       #50                      A <= 16; B <= 5; SRL_Select <= 0;
+       //Test SRA
+       #50 ALUControl <= SRA;   A <= 1; B <= 1; SRL_Select <= 0;
+       #50                      A <= -1; B <= 1; SRL_Select <= 0;
+       #50                      A <= 64; B <= 4; SRL_Select <= 0;
+       #50                      A <= -64; B <= 2; SRL_Select <= 0;
+       #50                      A <= 'hf0000000; B <= 28; SRL_Select <= 0;
+       //Test SRAV
+       #50 ALUControl <= SRAV;  A <= 1; B <= 1; SRL_Select <= 0;
+       #50                      A <= -1; B <= 1; SRL_Select <= 0;
+       #50                      A <= 64; B <= 4; SRL_Select <= 0;
+       #50                      A <= -64; B <= 2; SRL_Select <= 0;
+       #50                      A <= 'hf0000000; B <= 28; SRL_Select <= 0;
+       //Test SLTU
+       #50 ALUControl <= SLTU;  A <= 1; B <= 1; SRL_Select <= 0;
+       #50                      A <= 0; B <= 1; SRL_Select <= 0;
+       #50                      A <= 64; B <= 4; SRL_Select <= 0;
+       #50                      A <= 10; B <= 2048; SRL_Select <= 0;
+       #50                      A <= 1024; B <= 1000; SRL_Select <= 0;
+       //Test MUL
+       #50 ALUControl <= MUL;   A <= 1; B <= 1; SRL_Select <= 0;
+       #50                      A <= 0; B <= 1; SRL_Select <= 0;
+       #50                      A <= 64; B <= 4; SRL_Select <= 0;
+       #50                      A <= 10; B <= 2048; SRL_Select <= 0;
+       #50                      A <= 1024; B <= 1000; SRL_Select <= 0;
+       //Test MADD
+       #50 ALUControl <= MADD;  A <= 1; B <= 1; SRL_Select <= 0;
+       #50                      A <= 0; B <= 1; SRL_Select <= 0;
+       #50                      A <= 64; B <= 4; SRL_Select <= 0;
+       #50                      A <= 10; B <= 2048; SRL_Select <= 0;
+       #50                      A <= 1024; B <= 1000; SRL_Select <= 0;
+       //Test MSUB
+       #50 ALUControl <= MSUB;  A <= 1; B <= 1; SRL_Select <= 0;
+       #50                      A <= 0; B <= 1; SRL_Select <= 0;
+       #50                      A <= 64; B <= 4; SRL_Select <= 0;
+       #50                      A <= 10; B <= 2048; SRL_Select <= 0;
+       #50                      A <= 1024; B <= 1000; SRL_Select <= 0;
+       //Test SEH_SEB
+       #50 ALUControl <= SEH_SEB;  A <= 0; B <= 1; SRL_Select <= 0;
+       #50                         A <= 0; B <= 16; SRL_Select <= 0;
+       #50                         A <= 0; B <= 'h0000fff0; SRL_Select <= 0;
+       #50                         A <= 0; B <= 'h000000f0; SRL_Select <= 0;
+       #50                         A <= 0; B <= 'h00000080; SRL_Select <= 0;
 	end
 
 endmodule
