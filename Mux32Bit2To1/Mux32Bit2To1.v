e9c1180c9ebb392184359dcae7ad341d61b0195f
Mike Harmon <mpharmon@email.arizona.edu>
2016-10-10 13:19:22 -0700

    Revise I/O Names

--- Mux32Bit2To1/Mux32Bit2To1.v
+++ Mux32Bit2To1/Mux32Bit2To1.v
@@ -7,14 +7,13 @@
 // Description - Performs signal multiplexing between 2 32-Bit words.
 ////////////////////////////////////////////////////////////////////////////////
 
-module Mux32Bit2To1(out, inA, inB, sel);
-
-    output [31:0] out;
+module Mux32Bit2To1(Out, In0, In1, sel);
+    output [31:0] Out;
     
-    input [31:0] inA;
-    input [31:0] inB;
+    input [31:0] In0;
+    input [31:0] In1;
     input sel;
 
-    assign out = (sel) ? (inB):(inA);
+    assign out = (sel) ? (In1):(In0);
 
 endmodule
