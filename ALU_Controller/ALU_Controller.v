9e6a1cba6e7c6d566faf3f790ece6aea4b8c8e1d
Mike Harmon <mpharmon@email.arizona.edu>
2016-10-17 11:27:20 -0700

    Update for LW and SW Operations

--- ALU_Controller/ALU_Controller.v
+++ ALU_Controller/ALU_Controller.v
@@ -3,57 +3,56 @@
 module ALU_Controller(Rst, AluOp, Funct, ALUControl);
 
     input Rst;
-    input [3:0] AluOp;           //4 bit AluOp code sent from controller 
+    input [4:0] AluOp;           //4 bit AluOp code sent from controller 
     
     input [5:0] Funct;           //6 bit Instruction function field
     
     output reg [4:0] ALUControl; //5 bit output control signal sent to ALU
     
     //Controller AluOp 4 bit inputs (also are the state encodings)
-    localparam [3:0] DC =      'b0000, //DONT CARE
-                     ADD_I =   'b0001, //ADD IMMEDIATE
-                     SUB_I =   'b0010, //SUB IMMEDIATE
-                     OR_I =    'b0011, //OR IMMEDIATE
-                     AND_I =   'b0100, //AND IMMEDIATE
-                     XOR_I =   'b0101, //XOR IMMEDIATE
-                     NOR_I =   'b0110, //NOR IMMDEIATE
-                     ADDU_I =  'b0111, //ADDU IMMEDIATE
-                     SUBU_I =  'b1000, //SUBU IMMEDIATE
-                     MULTU_I = 'b1001, //MULTU IMMDEDIATE
-                     SLT_I =   'b1010, //SLT IMMEDIATE
-                     SLT_IU =  'b1011, //SLTU IMMEDIATE
-                     MUL_OP =  'b1100, //ALL MULTIPLY OPERATIONS
-                     SEBSEH_OP = 'b1101; //SIGN EXTEND OPERATIONS
-    
+    localparam [3:0] ALUOP_DC 		= 'b0000, // DONT CARE
+                     ALUOP_ADDI 	= 'b0001, // ADD IMMEDIATE
+                     ALUOP_SUBI 	= 'b0010, // SUB IMMEDIATE
+                     ALUOP_ORI 		= 'b0011, // OR IMMEDIATE
+                     ALUOP_ANDI 	= 'b0100, // AND IMMEDIATE (LW & SW TOO)
+                     ALUOP_XORI 	= 'b0101, // XOR IMMEDIATE
+                     ALUOP_ORI 		= 'b0110, // NOR IMMDEIATE
+                     ALUOP_ADDUI 	= 'b0111, // ADDU IMMEDIATE
+                     ALUOP_SUBUI 	= 'b1000, // SUBU IMMEDIATE
+                     ALUOP_MULTUI 	= 'b1001, // MULTU IMMDEDIATE
+                     ALUOP_SLTI 	= 'b1010, // SLT IMMEDIATE
+                     ALUOP_SLTIU 	= 'b1011, // SLTU IMMEDIATE
+                     ALUOP_MUL		= 'b1100, // ALL MULTIPLY OPERATIONS
+                     ALUOP_SE 		= 'b1101; // SIGN EXTEND OPERATIONS
+    				 
     //Instruction Function code 6 bit input definitions
     //---------------Dont Care FUNCTION FIELDS                
-    localparam [5:0] FC_add  =  'b100000,  //add
-                     FC_addu =  'b100001,  //addu
-                     FC_sub  =  'b100010,  //sub
-                     FC_mult =  'b011000,  //mult
-                     FC_multu=  'b011001,  //multu
-                     FC_and  =  'b100100,  //and
-                     FC_or   =  'b100101,  //or
-                     FC_nor  =  'b100111,  //nor
-                     FC_xor  =  'b100110,  //xor
-                     FC_sll  =  'b000000,  //sll
-                     FC_srl  =  'b000010,  //srl
-                     FC_sllv =  'b000100,  //sllv
-                     FC_slt  =  'b101010,  //slt
-                     FC_movn =  'b001011,  //movn
-                     FC_movz =  'b001010,  //movz
-                     FC_srlv =  'b000110,  //rotrv
-                     FC_sra  =  'b000011,  //sra
-                     FC_srav =  'b000111,  //srav
-                     FC_sltu =  'b101011,  //sltu
-     //---------------MUL FUNCTION FIELDS
-                     FC_mul  =  'b000010,  //mul
-                     FC_madd =  'b000000,  //madd
-                     FC_msub =  'b000100,  //msub
-                     FC_mfhi = 'b010000, // mfhi
-                     FC_mflo = 'b010010, // mflo
-                     FC_mthi = 'b010001, // mthi
-                     FC_mtlo = 'b010011; // mtlo                
+    localparam [5:0] FC_add  	=  'b100000,	// add
+                     FC_addu 	=  'b100001,	// addu
+                     FC_sub  	=  'b100010,	// sub
+                     FC_mult 	=  'b011000,	// mult
+                     FC_multu	=  'b011001,	// multu
+                     FC_and  	=  'b100100,	// and
+                     FC_or   	=  'b100101,	// or
+                     FC_nor  	=  'b100111,	// nor
+                     FC_xor  	=  'b100110,	// xor
+                     FC_sll  	=  'b000000,	// sll
+                     FC_srl  	=  'b000010,	// srl
+                     FC_sllv 	=  'b000100,	// sllv
+                     FC_slt  	=  'b101010,	// slt
+                     FC_movn 	=  'b001011,  	// movn
+                     FC_movz 	=  'b001010,  	// movz
+                     FC_srlv 	=  'b000110,  	// rotrv
+                     FC_sra  	=  'b000011,  	// sra
+                     FC_srav 	=  'b000111,  	// srav
+                     FC_sltu 	=  'b101011,  	// sltu
+                     FC_mul  	=  'b000010,  	// mul
+                     FC_madd 	=  'b000000,  	// madd
+                     FC_msub 	=  'b000100,  	// msub
+                     FC_mfhi 	= 	'b010000, 	// mfhi
+                     FC_mflo 	= 	'b010010, 	// mflo
+                     FC_mthi 	= 	'b010001, 	// mthi
+                     FC_mtlo 	= 	'b010011; 	// mtlo                
      
      //ALU control 5 bit output definitions                
     localparam [4:0] ADD  = 'b00000, // ADD  	 | 00000
@@ -78,7 +77,7 @@ module ALU_Controller(Rst, AluOp, Funct, ALUControl);
                      MUL  = 'b10011, // MUL      | 10011
                      MADD = 'b10100, // MADD     | 10100
                      MSUB = 'b10101, // MSUB     | 10101
-                     SEBSEH = 'b10110,// SEH_SEB  | 10110
+                     SE = 'b10110,// SEH_SEB | 10110
                      MFHI = 'b10111, // MFHI     | 10111
                      MFLO = 'b11000, // MFLO     | 11000
                      MTHI = 'b11001, // MTHI     | 11001
@@ -165,65 +164,59 @@ module ALU_Controller(Rst, AluOp, Funct, ALUControl);
             endcase
         end else begin //All immediate and non-dc operations are below
             case(AluOp) //First Check AluOp Code
-                ADD_I: begin
+                ALUOP_ADDI: begin
                     ALUControl <= ADD;
                 end
-                SUB_I: begin
+                ALUOP_SUBI: begin
                     ALUControl <= SUB;
                 end
-                OR_I: begin
+                ALUOP_ORI: begin
                     ALUControl <= OR;
                 end
-                AND_I: begin
+                ALUOP_ANDI: begin
                     ALUControl <= AND;
                 end
-                XOR_I: begin
+                ALUOP_XORI: begin
                     ALUControl <= XOR;
                 end
-                NOR_I: begin
+                ALUOP_NORI: begin
                     ALUControl <= NOR;
                 end
-                ADDU_I: begin
+                ALUOP_ADDUI: begin
                     ALUControl <= ADDU;
                 end
-                SUBU_I: begin
+                ALUOP_SUBUI: begin
                     ALUControl <= SUB;
                 end
-                MULTU_I: begin
+                ALUOP_MULTUI: begin
                     ALUControl <= MULT;
                 end
-                SLT_I: begin
+                ALUOP_SLTI: begin
                     ALUControl <= SLT;
                 end
-                SLT_IU: begin
+                ALUOP_SLTIU: begin
                     ALUControl <= SLT;
                 end
-                MUL_OP: begin
+                ALUOP_MUL: begin
                     if(Funct == FC_mul) begin
-//                    case(Funct)
-//                        FC_mul: begin  //mul
-                            ALUControl <= MUL;
-                        end
+                    	ALUControl <= MUL;
+                    end
                     else if(Funct == FC_madd) begin
-//                        FC_madd: begin  //madd
-                            ALUControl <= MADD;
-                        end
+                    	ALUControl <= MADD;
+                    end
                     else if(Funct == FC_msub) begin
-//                        FC_msub: begin   //msub
-                            ALUControl <= MSUB;
-                        end
+                        ALUControl <= MSUB;
+                    end
                     else begin
-//                        default: begin
-                            ALUControl <= ADD;
-                        end
-//                    endcase
+                        ALUControl <= ADD;
+                    end
                 end
-                SEBSEH_OP: begin
-                    ALUControl <= SEBSEH;
+                ALUOP_SE: begin
+                    ALUControl <= SE;
                 end
-                default: begin
-                    ALUControl <= ADD;
-                end
+                //default: begin
+                //    ALUControl <= ADD;
+                //end
             endcase
         end // End Else
     end
--- DatapathController/DatapathController.v
+++ DatapathController/DatapathController.v
@@ -1,37 +1,47 @@
 `timescale 1ns / 1ps
 
 module DatapathController(OpCode, RegDst, RegWrite, AluSrc, AluOp, MemWrite, MemRead, Branch, MemToReg, SignExt);
-    
     input[5:0] OpCode;
     
     output reg RegDst, RegWrite, AluSrc, MemWrite, MemRead, Branch, MemToReg, SignExt;
-  
     output reg [3:0] AluOp;
     
-    localparam [5:0] INITIAL = 'b111111,    //INITIAL
-                     OP_000000 = 'b000000,  //dont care/Non-immediate
-                     OP_011100 = 'b011100,  //multiplies
-                     OP_011111 = 'b011111,  //seh & seb
-                     OP_001001 = 'b001001,  //addiu
-                     OP_001000 = 'b001000,  //addi
-                     OP_001100 = 'b001100,  //andi
-                     OP_001101 = 'b001101,  //ori
-                     OP_001110 = 'b001110,  //xori
-                     OP_001010 = 'b001010,  //slti
-                     OP_001011 = 'b001011;  //sltui
+    localparam [5:0] INITIAL = 'b111111,    // INITIAL
+                     OP_000000 = 'b000000,  // Most R-type Instructions
+                     OP_011100 = 'b011100,  // multiplies
+                     OP_011111 = 'b011111,  // seh & seb
+                     OP_001001 = 'b001001,  // addiu
+                     OP_001000 = 'b001000,  // addi
+                     OP_001100 = 'b001100,  // andi
+                     OP_001101 = 'b001101,  // ori
+                     OP_001110 = 'b001110,  // xori
+                     OP_001010 = 'b001010,  // slti
+                     OP_001011 = 'b001011;  // sltui
+                     OP_101011 = 'b101011;  // SW
+                     OP_100011 = 'b100011;	// LW
+                     // NOT IMPLEMENTED YET...
+                     OP_101001 = 'b101001;	// SH
+                     OP_101000 = 'b101000;	// SB
+                     OP_100001 = 'b100001;	// LH
+                     OP_100000 = 'b100000;  // LB
      
     reg [5:0] State = INITIAL;
      
     //always @(change of any input)begin
     always @ (*) begin
         case(State)
-            INITIAL: begin
+            INITIAL: begin 
                 RegDst <= 0; RegWrite <= 0; AluSrc <= 0; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 0; AluOp <= 'b0001;
             end
-            OP_000000: begin
+            OP_000000: begin // Most R-type Instructions
                 RegDst <= 0; RegWrite <= 1; AluSrc <= 0; 
+                MemWrite <= 0; MemRead <= 0; Branch <= 0; 
+                MemToReg <= 0; SignExt <= 1; AluOp <= 'b0000;
+            end
+            OP_000010: begin // J
+            	RegDst <= 0; RegWrite <= 1; AluSrc <= 0; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 1; AluOp <= 'b0000;
             end
@@ -40,56 +50,87 @@ module DatapathController(OpCode, RegDst, RegWrite, AluSrc, AluOp, MemWrite, Mem
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 1; AluOp <= 'b1100;
             end
-            OP_011111: begin
+            OP_011111: begin // SEH & SEB
                 RegDst <= 0; RegWrite <= 1; AluSrc <= 0; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 0; AluOp <= 'b1101;
             end
-            OP_001001: begin
+            OP_001001: begin // ADDIU
                 RegDst <= 1; RegWrite <= 1; AluSrc <= 1; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 0; AluOp <= 'b0111;
             end
-            OP_001000: begin
+            OP_001000: begin // ADDI
                 RegDst <= 1; RegWrite <= 1; AluSrc <= 1; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 1; AluOp <= 'b0001;
             end
-            OP_001100: begin
+            OP_001100: begin // ANDI
                 RegDst <= 1; RegWrite <= 1; AluSrc <= 1; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 1; AluOp <= 'b0100;
             end
-            OP_001101: begin
+            OP_001101: begin // ORI
                 RegDst <= 1; RegWrite <= 1; AluSrc <= 1; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 1; AluOp <= 'b0011;
             end
-            OP_001110: begin
+            OP_001110: begin // XORI
                 RegDst <= 1; RegWrite <= 1; AluSrc <= 1; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 1; AluOp <= 'b0101;
             end
-            OP_001110: begin
+            /*OP_001110: begin // DUPLICATE
                 RegDst <= 1; RegWrite <= 1; AluSrc <= 1; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 1; AluOp <= 'b0101;
-            end
-            OP_001010: begin
+            end*/
+            OP_001010: begin // SLTI
                 RegDst <= 1; RegWrite <= 1; AluSrc <= 1; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 1; AluOp <= 'b1010;
             end
-            OP_001011: begin
+            OP_001011: begin //SLTUI
                 RegDst <= 1; RegWrite <= 1; AluSrc <= 1; 
                 MemWrite <= 0; MemRead <= 0; Branch <= 0; 
                 MemToReg <= 0; SignExt <= 1; AluOp <= 'b1011;
             end
-            default: begin
-                RegDst <= 0; RegWrite <= 0; AluSrc <= 0; 
-                MemWrite <= 0; MemRead <= 0; Branch <= 0; 
-                MemToReg <= 0; SignExt <= 0; AluOp <= 'b0001;
+            OP_101011: begin // SW
+            	RegDst <= 1; RegWrite <= 0; AluSrc <= 1;
+            	MemWrite <= 1; MemRead <= 0; Branch <= 0;
+            	MemToReg <= 1; SignExt <= 1; AluOp <= 'b0001; // Send ADDI to ALU Controller
             end
+            OP_100011: begin // LW
+            	RegDst <= 1; RegWrite <= 1; AluSrc <= 1;
+            	MemWrite <= 0; MemRead <= 1; Branch <= 0;
+            	MemToReg <= 1; SignExt <= 1; AluOp <= 'b0001; // Send ADDI to ALU Controller
+            end
+            // NEED TO FIND SOLUTION TO BE MEMORY READ/WRITE SAFE
+            OP_101001: begin // SH
+            	RegDst <= 1; RegWrite <= 0; AluSrc <= 1;
+            	MemWrite <= 1; MemRead <= 0; Branch <= 0;
+            	MemToReg <= 1; SignExt <= 1; AluOp <= 'b0001; // Send ADDI to ALU Controller
+            end
+        	OP_101000: begin // SB
+        		RegDst <= 1; RegWrite <= 0; AluSrc <= 1;
+            	MemWrite <= 1; MemRead <= 0; Branch <= 0;
+            	MemToReg <= 1; SignExt <= 1; AluOp <= 'b0001; // Send ADDI to ALU Controller
+        	end
+            OP_100001: begin // LH
+            	RegDst <= 1; RegWrite <= 1; AluSrc <= 1;
+            	MemWrite <= 0; MemRead <= 1; Branch <= 0;
+            	MemToReg <= 1; SignExt <= 1; AluOp <= 'b0001; // Send ADDI to ALU Controller
+            end
+            OP_100000: begin // LB
+            	RegDst <= 1; RegWrite <= 1; AluSrc <= 1;
+            	MemWrite <= 0; MemRead <= 1; Branch <= 0;
+            	MemToReg <= 1; SignExt <= 1; AluOp <= 'b0001; // Send ADDI to ALU Controller
+            end
+            //default: begin
+            //    RegDst <= 0; RegWrite <= 0; AluSrc <= 0; 
+            //    MemWrite <= 0; MemRead <= 0; Branch <= 0; 
+            //    MemToReg <= 0; SignExt <= 0; AluOp <= 'b0001;
+            //end
         endcase
      end
       //State Register
