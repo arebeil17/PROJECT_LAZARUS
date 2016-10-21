c8417f94e794d6b09fd64dda973bc1886a5bcb02
stealth07 <andres07@email.arizona.edu>
2016-10-21 15:44:47 -0700

    Updated for testing branch and jump
    I modified the IM for testing branch and jump with a basic MIPS program.

--- ProgramCounter/ProgramCounter.v
+++ ProgramCounter/ProgramCounter.v
@@ -37,7 +37,7 @@
          if(Reset == 1)begin
              PC <= 0;
              hold <= 1;
-         end else if(Address > 260)begin
+         end else if(Address > 24)begin
              PC <= 0;
              //hold <= 1;
          end else if(hold) begin
